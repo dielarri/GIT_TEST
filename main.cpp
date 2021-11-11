cea61cc5cdea4af11ce9be19e5c8d7f6b32be61c
Diego Arriaga <dielarri@ut.utm.edu>
2021-11-11 14:37:42 -0600

    asks about number and status ignores emacs backups, object files and bianaries

--- main.cpp
+++ main.cpp
@@ -3,5 +3,10 @@
 using namespace std;
 
 int main(){
-  cout << "Hello World" << endl;
+  cout << "please enter a number:" << endl;
+  int n;
+  cin >> n;
+  for (int i = 1; i < n + 1; i++){
+    cout << i << endl;
+  }
 }
