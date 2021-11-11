2ae76f77c42873af050201c3611239cd66ada9ee
Diego Arriaga <dielarri@ut.utm.edu>
2021-11-11 15:28:17 -0600

    working total

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ int main(){
   cout << "please enter a number:" << endl;
   int n;
   cin >> n;
-  int total;
+  int total = 0;
   for (int i = 1; i < n + 1; i++){
     cout << i << " ";
-    total += i;
+    total = total + i;
   }
   cout << total << endl;
 }
