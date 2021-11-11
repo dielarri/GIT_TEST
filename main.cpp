7ede3fdcaa7576efcacc58ead3b920eb8f97ef56
Diego Arriaga <dielarri@ut.utm.edu>
2021-11-11 14:40:57 -0600

    asks about number and returns total

--- main.cpp
+++ main.cpp
@@ -6,7 +6,10 @@ int main(){
   cout << "please enter a number:" << endl;
   int n;
   cin >> n;
+  int total;
   for (int i = 1; i < n + 1; i++){
-    cout << i << endl;
+    cout << i << " ";
+    total += i;
   }
+  cout << total << endl;
 }
